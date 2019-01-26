25a007d98d50e39067538a931606ad20210b9b61
esteban127 <esteban_127@hotmail.com>
2019-01-26 18:39:50 -0300

    fixing

--- Fantasmitas/Scenes/ghost/ghost.gd
+++ Fantasmitas/Scenes/ghost/ghost.gd
@@ -3,15 +3,19 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-export(int,"GHOST_BUTLER", "GHOST_MOTHER", "GHOST_COOK", "GHOST_DAUGHTER","GHOST_DOG", "GHOST_WIFE") var ghost_id
+enum Ghosts {GHOST_BUTLER, GHOST_MOTHER, GHOST_COOK, GHOST_DAUGHTER, GHOST_DOG, GHOST_WIFE, GHOST_ARTHUR}
+export(Ghosts) var ghost_id = 0
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	print("puto el que lee")
+	print(ghost_id)
+
 	pass # Replace with function body.
 
 func interact():
 	if Inventory.is_item_in_inventory(Quest.get_ghost_item(ghost_id)):
-		Quest.set_ghost_status(1)
+		Quest.set_ghost_status(ghost_id,1)
 		Inventory.remove_item_from_inventory(Quest.get_ghost_item(ghost_id))
 		Quest.set_fulfilled_ghost_request(ghost_id)
 		_change_state()
--- Fantasmitas/Scenes/rooms/scene_loader.gd
+++ Fantasmitas/Scenes/rooms/scene_loader.gd
@@ -2,8 +2,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	_spawn_character()
+	
+	pass
+	 # Replace with function body.
+	
+func _spawn_character():
+	var spawnpoint
+	for child in get_children():
+		if child is SpawnPoint and child.spawn_id == Quest.spawn_point:
+			spawnpoint = child.global_position
+	$Character.global_position = spawnpoint
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

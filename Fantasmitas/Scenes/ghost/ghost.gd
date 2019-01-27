a239194e53fbb6baa17c28057ebce90cebfc2018
esteban127 <esteban_127@hotmail.com>
2019-01-27 17:42:38 -0300

    Detroyd become human

--- Fantasmitas/Scenes/ghost/ghost.gd
+++ Fantasmitas/Scenes/ghost/ghost.gd
@@ -6,12 +6,27 @@ const quest = preload("res://Autoload/quest.gd")
 #enum Ghosts {GHOST_BUTLER, GHOST_MOTHER, GHOST_COOK, GHOST_DAUGHTER, GHOST_DOG, GHOST_WIFE, GHOST_ARTHUR}
 export(quest.Ghosts) var ghost_id = 0
 # Called when the node enters the scene tree for the first time.
-
 func _ready():
 	if Quest.get_fulfilled_ghost_request(ghost_id) and ghost_id == Quest.Ghosts.GHOST_BUTLER:
 		get_parent().get_node("Door2").is_active = true
+		if(Quest.get_ghost_status(ghost_id)== Quest.GhostStatus.GHOST_STATUS_HUMAN):
+			_become_human()
+		
 	pass # Replace with function body.
-
+func _become_human():
+	match ghost_id:
+		Quest.Ghosts.GHOST_BUTLER:
+			$AnimationPlayer.play("Butler")
+		Quest.Ghost.GHOST_MOTHER:
+			$AnimationPlayer.play("Mother")
+		Quest.Ghost.GHOST_COOK:
+			$AnimationPlayer.play("CooK")				
+		Quest.Ghost.GHOST_DOG:
+			$AnimationPlayer.play("Dog")
+		Quest.Ghost.GHOST_WIFE:
+			$AnimationPlayer.play("Wife")
+					
+					
 func interact():
 	if(Quest.get_fulfilled_ghost_request(ghost_id)):
 		return
@@ -35,11 +50,12 @@ func _change_state():
 	pass
 
 func fade_ended():
+	_become_human()
 	if ghost_id == Quest.Ghosts.GHOST_BUTLER:
 		get_parent().get_node("Door2").is_active = true
 	if(ghost_id == Quest.Ghosts.GHOST_WIFE):
 		Quest.add_current_ghost_quests(Quest.Ghosts.GHOST_ARTHUR)
-		var item = get_parent().get_tree().get_nodes_in_group("item")
+		var item = get_parent().get_tree().get_nodes_in_group("item")		
 		item[0].enabled = true
 	pass
 func midle_fade():
